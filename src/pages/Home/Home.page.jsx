922e6e307de652bf5b248295f2ada16aace4c237
harshranpariya <harshranpariya@gmail.com>
2021-07-29 19:18:04 -0400

    create route and sample menu

---
+++ src/pages/Home/Home.page.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+
+import Menu from '../../components/Menu/Menu.page';
+
+const Home = () => {
+    return(
+        <>
+            <Menu/>
+        </>
+    );
+}
+
+
+export default Home;
