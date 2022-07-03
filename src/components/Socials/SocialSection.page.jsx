adf8b9a2adb457b80ce20609b3f547d0d439acb3
jjcorsino <ekko.-@outlook.com>
2022-07-02 21:18:42 -0300

    fixs

--- src/components/Socials/SocialSection.page.jsx
+++ src/components/Socials/SocialSection.page.jsx
@@ -4,6 +4,14 @@ import './SocialSection.style.scss'
 const SocialSection = () => {
   return (
     <div className="social-section">
+      <div className="social-text"><h5>Visit Other Ubisoft Channels</h5></div>
+      <div className="social-icons">
+        <a href="https://twitter.com/Ubisoft" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+        <a href="https://www.facebook.com/Ubisoft/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
+        <a href="htttps//www.twitch.tv/ubisoft" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitch"></i></a>
+        <a href="https://www.youtube.com/user/Ubisoft" target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube"></i></a>
+        <a href="https://www.instagram.com/ubisoft/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+      </div>
     </div>
   )
 }
